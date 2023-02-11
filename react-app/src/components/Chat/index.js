eab873ab0db86f78f5faa332c87b57ba5cbd2063
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-02-11 11:43:20 -0800

    added socket info

--- react-app/src/components/Chat/index.js
+++ react-app/src/components/Chat/index.js
@@ -4,8 +4,36 @@ import { io } from "socket.io-client";
 let socket;
 
 const Chat = () =>{
+    // use state to store all messages received so we can display them
+    const [messages, setMessages] = useState([])
+    //use state for controlled form input
+    const [chatInput, setChatInput] = useState('')
 
+    //open socket connection w/ useeffect
+    useEffect(()=>{
+        socket = io()
+        //listen for chat events
+        socket.on("chat", (chat)=>{
+            //add new chat into our messages array
+            setMessages(messages => [...messages, chat])
+        })
 
+        //when component unmounts, disconnect
+        return (()=>{
+            socket.disconnect()
+        })
+    },[])
+    
+
+    return(
+        <form onSubmit={sendChat}>
+            <input
+                value={chatInput}
+                onChange={updateChatInput}
+            />
+            <button type="submit">Send</button>
+        </form>
+    )
 }
 
 export default Chat;

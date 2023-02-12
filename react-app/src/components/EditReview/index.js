39065977e9c83267dfecc30596951c242d08459e
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-02-12 00:27:35 -0800

    reviews fixed

--- react-app/src/components/EditReview/index.js
+++ react-app/src/components/EditReview/index.js
@@ -4,41 +4,29 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { editReviewThunk } from "../../store/reviews";
 // have this render on the ReviewById component
-const EditReview = ({reviewId}) => {
+const EditReview = ({ reviewId }) => {
 	const dispatch = useDispatch();
 	const history = useHistory();
 	const [reviewBody, setReviewBody] = useState("");
 	const [stars, setStars] = useState("");
 	const [errors, setErrors] = useState([]);
 	const { userId } = useParams(); // userId of PM
-    const sessionUser = useSelector((state=> state.session.user))
+	const sessionUser = useSelector((state) => state.session.user);
 	const user = useSelector((state) => state.users[userId]);
-    const review = useSelector((state) => state?.reviews[reviewId]);
-	const userReview = useSelector((state) => state.user.review.id)
-	console.log(userReview, "userReview")
-	// console.log(review, "REVIEW IN TEH REVIEW COMP")
+	const review = useSelector((state) => state.reviews[reviewId]);
 
-    const reviews = useSelector((state) => Object.values(state.reviews))
-    // console.log(reviews, "THESE ARE THE REVIEWS")
-    //look at array of sessionUserReview
+	const reviews = useSelector((state) => Object.values(state.reviews));
+	// //(reviews, "THESE ARE THE REVIEWS")
+	//look at array of sessionUserReview
 	const sessionUserReview = reviews.filter(
 		(review) => review.reviewerId == sessionUser.id
 	);
-	console.log(sessionUserReview, "sessionUserReview")
-    const seshReviewId =  sessionUserReview[0]['id']
-    // console.log(seshReviewId, "SESHReviewID")
+	// //(sessionUserReview, "sessionUserReview")
+	const seshReviewId = sessionUserReview[0]["id"];
+	// //(seshReviewId, "SESHReviewID")
 	const sessionUserId = useSelector((state) => state.session.user.id);
-    const reviewUserId = sessionUserReview[0]['reviewerId']
-    // [{…}]0: {id: 5, reviewBody: 'amazing service!', reviewerId: 2, stars: '3', user: {…}, …}length: 1[[Prototype]]: Array(0) 'SESSIONUSERREVIEW'
-
-	useEffect(() => {
-		if (sessionUserReview) {
-			setReviewBody(sessionUserReview.reviewBody);
-			setStars(sessionUserReview.stars);
-		}
-	}, [review]);
-
-
+	const reviewUserId = sessionUserReview[0]["reviewerId"];
+	// [{…}]0: {id: 5, reviewBody: 'amazing service!', reviewerId: 2, stars: '3', user: {…}, …}length: 1[[Prototype]]: Array(0) 'SESSIONUSERREVIEW'
 
 	const handleEdit = async (e) => {
 		e.preventDefault();
@@ -50,7 +38,7 @@ const EditReview = ({reviewId}) => {
 			review_body: reviewBody,
 			stars,
 		};
-		// console.log(editedReview, "THIS IS THE EDITED REVIEW");
+		// //(editedReview, "THIS IS THE EDITED REVIEW");
 		let data = await dispatch(editReviewThunk(editedReview));
 		if (data) {
 			setErrors(data);
@@ -75,17 +63,17 @@ const EditReview = ({reviewId}) => {
 				<div>Review</div>
 				<input
 					type="text"
-					placeholder={sessionUserReview[0]['reviewBody']}
+					placeholder={sessionUserReview[0]["reviewBody"]}
 					required
-					value={review?.reviewBody}
+					value={reviewBody}
 					onChange={(e) => setReviewBody(e.target.value)}
 				/>
 				<div>Rating (1 to 5)</div>
 				<input
 					type="text"
-					placeholder={sessionUserReview[0]['stars']}
+					placeholder={sessionUserReview[0]["stars"]}
 					required
-					value={review?.stars}
+					value={stars}
 					onChange={(e) => setStars(e.target.value)}
 				/>
 

c960e14f34cbf8c34db6637925b2af110548c48e
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-02-13 07:16:17 -0800

    took out remaining print in review_routes

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -41,7 +41,7 @@ def reviews():
 @login_required
 def update_review(id):
     review = Review.query.get(id)
-    print(review, "this is the review in the BE route")
+    # print(review, "this is the review in the BE route")
     form = ReviewForm()
 
     if form.data["reviewer_id"] != current_user.id:
